0c2e25cc6bff4225484fd5996a34b1a1c32fba06
varshashaji3 <varshashaji2025@mca.ajce.in>
2024-10-14 06:52:40 +0530

    new commit6

--- client/context_processors.py
+++ client/context_processors.py
@@ -1,12 +1,16 @@
 from client.models import ClientProfile
 from core.models import CustomUser, Register
 
+
 def client_context(request):
     if request.user.is_authenticated and request.user.role == 'client':
         uid = request.user.id
         profile1 = CustomUser.objects.get(id=uid)
         profile2 = Register.objects.get(user_id=uid)
-        client = ClientProfile.objects.get(user_id=uid)
+        try:
+            client = ClientProfile.objects.get(user_id=uid)
+        except ClientProfile.DoesNotExist:
+            client = None  
         return {
             'profile1': profile1,
             'profile2': profile2,
--- freelancer/context_processors.py
+++ freelancer/context_processors.py
@@ -1,12 +1,17 @@
 from freelancer.models import FreelancerProfile
 from core.models import CustomUser, Register
-
+from django.shortcuts import get_object_or_404
 def freelancer_context(request):
     if request.user.is_authenticated and request.user.role == 'freelancer':
         uid = request.user.id
-        profile1 = CustomUser.objects.get(id=uid)
-        profile2 = Register.objects.get(user_id=uid)
-        freelancer = FreelancerProfile.objects.get(user_id=uid)
+        profile1 = get_object_or_404(CustomUser, id=uid)
+        profile2 = get_object_or_404(Register, user_id=uid)
+        
+        try:
+            freelancer = FreelancerProfile.objects.get(user_id=uid)
+        except FreelancerProfile.DoesNotExist:
+            freelancer = None  # or handle the case when it doesn't exist
+        
         return {
             'profile1': profile1,
             'profile2': profile2,
